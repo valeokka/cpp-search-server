e347706771c8be9db632cc4af97765e003656b1b
valeokka <swekarochi@gmail.com>
2022-11-01 00:26:03 +0300

    Decision was added

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,29 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Проверяем есть ли в числе 3
+bool CheckForThree(int i) {
+	string k = to_string(i);
+	for (const char& number : k) {
+		if (number == '3') {
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+
+	int counter = 0;
+	for (int i = 1000; i != 0; --i) {
+		bool checker = CheckForThree(i);
+		if (checker == true)
+			++counter;
+	}
+	cout << counter; // В 271 числе
+}
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
