61e01c9859b76925240eaa0efc165a67ed71ae6c
valeokka <swekarochi@gmail.com>
2023-01-27 12:37:02 +0300

    improved

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -4,38 +4,42 @@
 #include <deque>
 #include "search_server.h"
 
-class RequestQueue {
+class RequestQueue
+{
 public:
-    explicit RequestQueue(const SearchServer& search_server) 
-    : search_server(search_server)
-    {    
-}
-    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
-    template <typename DocumentPredicate>
-    void AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        std::vector <Document> result = search_server.FindTopDocuments(raw_query, document_predicate);
-        PushDocument(result);
+    explicit RequestQueue(const SearchServer &search_server)
+        : search_server(search_server)
+    {
     }
-    void AddFindRequest(const std::string& raw_query, DocumentStatus status);
 
-    void AddFindRequest(const std::string& raw_query);
+    void AddFindRequest(const std::string &raw_query, DocumentStatus status);
+
+    void AddFindRequest(const std::string &raw_query);
 
     int GetNoResultRequests() const;
+
 private:
-    struct QueryResult {
-        std::vector <Document> documents;
-        
+    struct QueryResult
+    {
+        std::vector<Document> documents;
+
         QueryResult(std::vector<Document> doc)
-        : documents (doc)
+            : documents(doc)
         {
         }
     };
     int requests_on_board_ = 0;
     std::deque<QueryResult> requests_;
     std::deque<QueryResult> requests_successful_;
-     const SearchServer& search_server;
+    const SearchServer &search_server;
     const static int min_in_day_ = 1440;
-    // возможно, здесь вам понадобится что-то ещё
 
-    void PushDocument(const std::vector<Document>& result);
+    void PushDocument(const std::vector<Document> &result);
+
+    template <typename DocumentPredicate>
+    void AddFindRequest(const std::string &raw_query, DocumentPredicate document_predicate)
+    {
+        std::vector<Document> result = search_server.FindTopDocuments(raw_query, document_predicate);
+        PushDocument(result);
+    }
 };
